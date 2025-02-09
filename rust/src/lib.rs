dcf8b969de8818d7c2e4822229db0b4f1a1f9d65
TO <th.theophyl.hoguet@gmail.com>
2025-02-09 03:15:59 +0100

    first grid

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,16 +1,38 @@
-#[derive(PartialEq)]
-pub enum Cell_status {
+#[derive(PartialEq, Debug)]
+pub enum PlayerSymbol {
     X,
     O,
     EMPTY,
 }
+
+impl Clone for PlayerSymbol {
+    fn clone(&self) -> PlayerSymbol {
+        *self
+    }
+}
+
+impl Copy for PlayerSymbol {}
+
+#[derive(Debug)]
 pub struct Coordinate {
     x: u8,
     y: u8,
 }
 
 impl Coordinate {
-    fn is_in(&self, x: u8, y: u8) -> bool {
+    pub fn is_in(&self, x: u8, y: u8) -> bool {
         self.x == x && self.y == y
+    }
+
+    pub fn get_x(&self) -> u8 {
+        self.x
+    }
+
+    pub fn get_y(&self) -> u8 {
+        self.y
+    }
+
+    pub fn new(x: u8, y: u8) -> Coordinate {
+        Coordinate { x, y }
     }
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,18 @@
+use rust::{Coordinate, PlayerSymbol};
+use shapes::shapes::{Draw, Grid};
+
 mod shapes;
 
 fn main() {
-    println!("Hello, world!");
+    let mut grid = Grid::new();
+    println!("{:?}", grid.draw());
+    grid.play(Coordinate::new(0, 0), PlayerSymbol::X);
+    println!("{:?}", grid.draw());
+    println!("{:?}", grid.is_win());
+    grid.play(Coordinate::new(0, 1), PlayerSymbol::X);
+    println!("{:?}", grid.draw());
+    println!("{:?}", grid.is_win());
+    grid.play(Coordinate::new(0, 2), PlayerSymbol::X);
+    println!("{:?}", grid.draw());
+    println!("{:?}", grid.is_win());
 }
